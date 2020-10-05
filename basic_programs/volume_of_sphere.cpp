9fc72a18a20af0a3f5d9b65641259cc054359ed9
computerbeasts <72040712+computerbeasts@users.noreply.github.com>
2020-10-05 18:34:51 +0530

    Add files via upload

---
+++ basic_programs/volume_of_sphere.cpp
@@ -0,0 +1,29 @@
+/*
+=============================================
+
+
+
+
+
+
+=============================================
+*/
+
+#include <iostream>
+
+using namespace std ;
+
+int main() {
+	
+	//volume of sphere = 4 / 3* PI r^3
+	
+	int radius = 48 ;
+	const  PI = 3.14 ;
+	
+	float volumeOfSphere = ( 4.0 / 3.0) * 3.14 * ( radius * radius * radius) ;
+	
+	cout << " Volume Of Sphere is " << volumeOfSphere << endl ;
+	
+	
+	return 0 ;
+}
